27f557837095d0e32120d410bfe75d7ac19a994d
rmahesh <rmahesh@micron.com>
2025-06-12 14:35:15 -0600

    Another attempt

--- ProcessCodeProject.py
+++ ProcessCodeProject.py
@@ -191,7 +191,7 @@ def load_data_from_sharepoint():
         field_mapping = {
             'Segment': 'Segment',
             'Supplier': 'Supplier',
-            'Component_Generation': 'Component_Generation',
+            'Component_Generation': 'Product_x0020_Family',
             'Revision': 'REV',
             'Component_Type': 'Title',
             'Process_Code': 'Process_x0020_Code',
