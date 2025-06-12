2b3df7933ccee8fe16a7418e21f51cc76e9af028
rmahesh <rmahesh@micron.com>
2025-06-12 16:16:38 -0600

    str and upper this should be good to go

--- ProcessCodeProject.py
+++ ProcessCodeProject.py
@@ -991,7 +991,7 @@ def main():
                                     
                                     # Capitalize all string columns
                                     for col in parts_df.columns:
-                                        parts_df[col] = parts_df[col].astype(str).str.upper()
+                                        parts_df[col] = parts_df[col].apply(lambda x: str(x).upper())
                                     
                                     # Display the DataFrame as a table
                                     st.table(parts_df)
